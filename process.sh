ad898ed4078f68f5f3b0790e58d1aa3315412d9e
Brent Jones <bjones@stlpublicradio.org>
2015-06-25 17:41:43 -0500

    add race breakdown for stl city

--- process.sh
+++ process.sh
@@ -8,6 +8,7 @@ echo "Cleaning up old output"
 rm temp/*
 rm output/*
 rm 2015-06-24-census.db
+rm 2015-06-24-census-race.db
 
 echo "Activating virtualenv"
 workon 2015-06-24-census
@@ -43,6 +44,17 @@ cat sql/race_query_stl_age.sql | sqlite3 -csv -header 2015-06-24-census.db > out
 
 echo "Running region race/age query"
 cat sql/race_query_region_age.sql | sqlite3 -csv -header 2015-06-24-census.db > output/region_race_age.csv
+
+# Age group breakdown by race
+echo "Getting STL race stats"
+csvgrep -c 3 -r "510" temp/region.csv | csvcut -c 6-8,35-46,57,58 | tail -n +2 > temp/stl_race_breakdown.csv
+
+echo "Creating database"
+cat sql/race_data_create.sql | sqlite3 2015-06-24-census-race.db
+echo ".import temp/stl_race_breakdown.csv race_data" | sqlite3 -csv 2015-06-24-census-race.db
+
+echo "Running stl race/age breakdown"
+cat sql/race_query_stl_age_breakdown.sql | sqlite3 -csv -header 2015-06-24-census-race.db > output/stl_race_breakdown.csv
 
 echo "Deactivating virtualenv"
 deactivate
